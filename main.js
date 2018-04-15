10509cebcfe2c71faad204eb255e633839880fcc
Darkmift <Darkmift@users.noreply.github.com>
2018-04-16 00:52:14 +0300

    ajax with some animation

--- main.js
+++ main.js
@@ -2,11 +2,25 @@ console.log('js online');
 
 //define a links in navbar
 $('#Home,#Students,#Courses').click(function(e) {
+    var container = $('#container').css('height', 0);
+    var content;
+    //prevent click behaviour or redirect
     e.preventDefault();
     console.log('Link disabled');
-    $(this).attr('id');
-    if (!$('#container').empty()) {
-        $('#container').empty();
+    //assign id of clicked
+    var thisId = $(this).attr('id');
+    console.log(thisId);
+    //stop previous anaimation if in progress
+    if (container.is(':animated')) {
+        console.log('animation in progress');
+        container.stop();
     }
-    $('#container').load($(this).attr('id') + '.html');
+    container.animate({ height: "600px" }, 1500, "linear");
+
+    $.get(thisId + '.html', function(data) {
+        container.html(data);
+    });
+
 });
+
+// $(this).show("slide", { direction: "left" }, 1000);
